bb3fdeea06fffeea6d83ea8c0a997ee20bc25235
lim <tbnsok40@naver.com>
2020-10-03 22:38:31 +0900

    [lim] | backup용

--- myproject/post/forms.py
+++ myproject/post/forms.py
@@ -1,13 +1,21 @@
 from django.forms import ModelForm
 
-from .models import Post,Comment
+from .models import Post, Comment
+
+from django import forms
+
 
 class PostForms(ModelForm):
     class Meta:
         model = Post
 
-        fields = ('title',"desc", 'image')
-    
+        fields = ('title', "desc", 'image')
+
+        widget = {
+            'title': forms.TextInput(attrs={'class': 'temp'})
+        }
+
+
 class CommentForms(ModelForm):
     class Meta:
         model = Comment
